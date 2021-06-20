533308b856c630fd037eb6072378432ea90444a7
Anton Zimin <an.zim.learn@gmail.com>
2021-06-20 13:34:32 +0300

    added README. type checking deleted

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project for upgrade my skills
+
+[![Build Status](https://github.com/zimkaa/my_big_project/actions/workflows/checks.yaml/badge.svg?branch=master)](https://github.com/zimkaa/my_big_project/actions/workflows/checks.yaml)
--- request_to_nl.py
+++ request_to_nl.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # type: ignore
 from datetime import datetime
 import requests
 
@@ -8,16 +8,16 @@ import config
 
 
 def send_telegram(text: str) -> None:
-    token = config.TG_TOKEN
+    token = config.TG_TOKEN  # type: ignore
     url = "https://api.telegram.org/bot"
-    channel_id = config.CHANNEL_ID
-    url += token
-    method = url + "/sendMessage"
+    channel_id = config.CHANNEL_ID  # type: ignore
+    url += token  # type: ignore
+    method = url + "/sendMessage"  # type: ignore
     query = requests.post(
         method,
         data={
-            "chat_id": channel_id,
-            "text": text,
+            "chat_id": channel_id,  # type: ignore
+            "text": text,  # type: ignore
         },
     )
     if query.status_code != 200:
@@ -28,8 +28,9 @@ def my_ip(proxies: dict = None) -> str:
     """
     Get IP (need to chek proxye)
     """
-    answer = requests.get(config.CHECKER_IP_SITE,
-                          headers=config.HEADER, proxies=proxies)
+    answer = requests.get(
+        config.CHECKER_IP_SITE,
+        headers=config.HEADER, proxies=proxies)  # type: ignore
     if answer.status_code != 200:
         text = "PROXY DON'T RESPONSE!!!"
         send_telegram(text)
@@ -90,8 +91,7 @@ def log_in(my_connect: requests.Session) -> requests.models.Response:
     get_html(my_connect, config.URL, config.HEADER, config.PROXYES)
     post_html(
         my_connect, config.URL_GAME, config.HEADER,
-        config.PROXYES, config.DATA,
-    )
+        config.PROXYES, config.DATA)  # type: ignore
     html = get_html(
         my_connect, config.URL_MAIN, config.HEADER, config.PROXYES,
     )
